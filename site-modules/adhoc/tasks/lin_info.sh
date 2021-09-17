f68aee06345045049e75e939d4517e709bb4b42e
Liam Sexton <liam.sexton@puppet.com>
2021-09-17 16:09:44 +0100

    what

--- site-modules/adhoc/tasks/lin_info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-info=$(lscpu)
-echo -e "System Info:\n \n$info"
-echo "Your custom parameter was: ${PT_myparam1}"
--- site-modules/adhoc/tasks/win_info.ps1
+++
@@ -1,4 +0,0 @@
-Param($myparam1)
-$systeminfo = systeminfo
-Write-Output "System Info:" $systeminfo
-Write-Output "Your custom parameter was: ${myparam1}"
